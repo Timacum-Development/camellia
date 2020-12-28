0765f8948454b88dea55704984a420ad015901a1
jovanaj97 <jovana@timacum.com>
2020-12-28 12:55:37 +0100

    Font

--- src/shared/GeneralsStyles.ts
+++ src/shared/GeneralsStyles.ts
@@ -1,6 +1,31 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+	@font-face {
+    font-family: 'geomanistitalic';
+    src: url('geomanist-regular-italic-webfont.eot');
+    src: url('geomanist-regular-italic-webfont.eot?#iefix') format('embedded-opentype'),
+         url('geomanist-regular-italic-webfont.woff2') format('woff2'),
+         url('geomanist-regular-italic-webfont.woff') format('woff'),
+         url('geomanist-regular-italic-webfont.ttf') format('truetype'),
+         url('geomanist-regular-italic-webfont.svg#geomanistitalic') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+	}
+
+	@font-face {
+		font-family: 'geomanistregular';
+		src: url('geomanist-regular-webfont.eot');
+		src: url('geomanist-regular-webfont.eot?#iefix') format('embedded-opentype'),
+			url('geomanist-regular-webfont.woff2') format('woff2'),
+			url('geomanist-regular-webfont.woff') format('woff'),
+			url('geomanist-regular-webfont.ttf') format('truetype'),
+			url('geomanist-regular-webfont.svg#geomanistregular') format('svg');
+		font-weight: normal;
+		font-style: normal;
+
+	}
 
 	html, body, #root {
 		height: 100%;
