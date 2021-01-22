75415b2742b6ad6edf879f485fbff40e698b6dfb
milan-timacum <milan@timacum.com>
2021-01-22 14:15:06 +0100

    Deploy fix

--- src/containers/general/Footer/Footer.tsx
+++ src/containers/general/Footer/Footer.tsx
@@ -1,50 +1,49 @@
-import React from "react";
+import React from 'react';
 import {
-  FooterWrap,
-  FooterInfo,
-  FooterNav,
-  FooterLine,
-  LineTop,
-} from "./style";
+	FooterWrap,
+	FooterInfo,
+	FooterNav,
+	FooterLine,
+	LineTop,
+} from './style';
 
-import Logo from "../../../shared/components/Logo";
-import SocialMedia from "../../../shared/components/Social";
-import Link from "../../../shared/components/Link";
-import { FooterLink } from "../../../shared/components/Link/style";
+import Logo from '../../../shared/components/Logo';
+import SocialMedia from '../../../shared/components/Social';
+import Link from '../../../shared/components/Link';
 
 const Footer = () => {
-  return (
-    <FooterWrap>
-      <LineTop />
-      <SocialMedia style={{ paddingTop: "100px" }} />
-      <FooterInfo>
-        <Logo
-          style={{ flexDirection: "row-reverse", marginBottom: "43px" }}
-          styleName={{ marginRight: "11px" }}
-        />
-        <FooterNav>
-          <ul>
-            <li>
-              <Link text="Home" />
-            </li>
-            <li>
-              <Link text="Shop" />
-            </li>
-            <li>
-              <Link text="Products" />
-            </li>
-            <li>
-              <Link text="Services" />
-            </li>
-            <li>
-              <Link text="Contact" />
-            </li>
-          </ul>
-        </FooterNav>
-      </FooterInfo>
-      <FooterLine />
-    </FooterWrap>
-  );
+	return (
+		<FooterWrap>
+			<LineTop />
+			<SocialMedia style={{ paddingTop: '100px' }} />
+			<FooterInfo>
+				<Logo
+					style={{ flexDirection: 'row-reverse', marginBottom: '43px' }}
+					styleName={{ marginRight: '11px' }}
+				/>
+				<FooterNav>
+					<ul>
+						<li>
+							<Link text='Home' />
+						</li>
+						<li>
+							<Link text='Shop' />
+						</li>
+						<li>
+							<Link text='Products' />
+						</li>
+						<li>
+							<Link text='Services' />
+						</li>
+						<li>
+							<Link text='Contact' />
+						</li>
+					</ul>
+				</FooterNav>
+			</FooterInfo>
+			<FooterLine />
+		</FooterWrap>
+	);
 };
 
 export default Footer;
