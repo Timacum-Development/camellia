3ad1bfd4df4ee58429ac04a989c70446b22e6f0a
milan-timacum <milan@timacum.com>
2021-01-22 14:19:07 +0100

    Deploy fix

--- src/shared/components/Card/style.ts
+++ src/shared/components/Card/style.ts
@@ -1,89 +1,89 @@
-import styled, { keyframes } from "styled-components";
-import { color } from "../../styles";
+import styled from 'styled-components';
+import { color } from '../../styles';
 
 export const FlowerCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 25%;
-  transition: 0.1s ease-in-out;
-  position: relative;
-  z-index: 200;
-  background: linear-gradient(to bottom, transparent 30%, ${color.white} 70%);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 25%;
+	transition: 0.1s ease-in-out;
+	position: relative;
+	z-index: 200;
+	background: linear-gradient(to bottom, transparent 30%, ${color.white} 70%);
 
-  &:after {
-    content: "";
-    width: 100%;
-    height: 90%;
-    position: absolute;
-    top: 70px;
-    background-color: ${color.white};
-    box-shadow: 0 40px 50px -50px ${color.black};
-    z-index: -1;
-    visibility: hidden;
-    opacity: 0;
-    transition: 0.25s ease-in-out;
-  }
+	&:after {
+		content: '';
+		width: 100%;
+		height: 90%;
+		position: absolute;
+		top: 70px;
+		background-color: ${color.white};
+		box-shadow: 0 40px 50px -50px ${color.black};
+		z-index: -1;
+		visibility: hidden;
+		opacity: 0;
+		transition: 0.25s ease-in-out;
+	}
 
-  &:hover {
-    &:after {
-      visibility: visible;
-      opacity: 1;
-    }
-  }
+	&:hover {
+		&:after {
+			visibility: visible;
+			opacity: 1;
+		}
+	}
 `;
 
 export const FlowerImg = styled.img`
-  display: block;
+	display: block;
 `;
 
 export const FlowerText = styled.div`
-  position: relative;
-  margin-bottom: 57px;
+	position: relative;
+	margin-bottom: 57px;
 
-  &:after {
-    content: "";
-    width: 221px;
-    height: 2px;
-    background-color: ${color.grey};
-    position: absolute;
-    bottom: -25px;
-    left: 50%;
-    transform: translateX(-50%);
-  }
+	&:after {
+		content: '';
+		width: 221px;
+		height: 2px;
+		background-color: ${color.grey};
+		position: absolute;
+		bottom: -25px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
 `;
 
 export const IconWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 export const FlowerName = styled.h3`
-  font-size: 3.125rem;
-  color: ${color.darkPurple};
-  text-align: center;
-  margin-bottom: 15px;
+	font-size: 3.125rem;
+	color: ${color.darkPurple};
+	text-align: center;
+	margin-bottom: 15px;
 `;
 
 export const FlowerPrice = styled.h5`
-  font-size: 0.9375rem;
-  color: ${color.darkPurple};
-  opacity: 0.5;
-  text-align: center;
+	font-size: 0.9375rem;
+	color: ${color.darkPurple};
+	opacity: 0.5;
+	text-align: center;
 `;
 
 export const BuyNow = styled.a`
-  font-size: 0.8125rem;
-  font-family: "Roboto Slab", sans-serif;
-  font-weight: bold;
-  text-transform: uppercase;
-  color: ${color.blue};
-  letter-spacing: 2px;
-  margin-bottom: 40px;
+	font-size: 0.8125rem;
+	font-family: 'Roboto Slab', sans-serif;
+	font-weight: bold;
+	text-transform: uppercase;
+	color: ${color.blue};
+	letter-spacing: 2px;
+	margin-bottom: 40px;
 `;
 
 export const BuyImg = styled.img`
-  display: block;
-  margin-bottom: 13px;
+	display: block;
+	margin-bottom: 13px;
 `;
--- src/shared/components/Logo/style.ts
+++ src/shared/components/Logo/style.ts
@@ -1,5 +1,4 @@
-import styled, { keyframes } from "styled-components";
-import { color } from "../../styles";
+import styled, { keyframes } from 'styled-components';
 
 export const LogoAnimation = keyframes`
   0%{
@@ -13,21 +12,21 @@ export const LogoAnimation = keyframes`
 `;
 
 export const LogoWrap = styled.div`
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
 
-  &:hover {
-    animation: ${LogoAnimation} 0.5s ease-in-out forwards;
-  }
+	&:hover {
+		animation: ${LogoAnimation} 0.5s ease-in-out forwards;
+	}
 `;
 
 export const LogoFlower = styled.img`
-  display: block;
-  width: 57px;
-  height: 57px;
-  margin-right: 16px;
+	display: block;
+	width: 57px;
+	height: 57px;
+	margin-right: 16px;
 `;
 
 export const LogoName = styled.img`
-  display: block;
+	display: block;
 `;
